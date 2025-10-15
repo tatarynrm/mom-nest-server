1caed2a82f116e92285c3884eaa079e88954ba7a
tatarynrm <tatarynrm@gmail.com>
2025-10-15 12:52:34 +0300

    ewqewq

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Вмикаємо CORS
-  const allowedOrigins = ['http://localhost:3000', 'https://myapp.com'];
+  const allowedOrigins = [
+    'http://localhost:3000',
+    'http://localhost:3009',
+    'http://91.239.235.132:3009',
+    'https://mira-notes.site',
+  ];
   app.enableCors({
     origin: (origin, callback) => {
       if (!origin || allowedOrigins.includes(origin)) {
